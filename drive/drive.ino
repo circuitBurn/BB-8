52c5bbca33bba0f648b6023e780c14ff9d3bad1c
Patrick Ryan <pat.m.ryan@gmail.com>
2021-09-21 18:28:39 -0400

    S2S and audio update

--- drive/drive.ino
+++ drive/drive.ino
@@ -34,49 +34,54 @@
 // Flywheel
 #define FLY_R_PWM 8
 #define FLY_L_PWM 9
-#define FLY_EN 29
 
 // Side to side
 #define S2S_R_PWM 6
 #define S2S_L_PWM 7
 #define S2S_EN 33
 #define S2S_POT A0
-#define maxS2STilt 20
+#define maxS2STilt 25
 //#define reverseS2SPot
-#define S2SEase 2.5
+#define S2SEase 5.5
 
-// TODO:
+/**
+ * Gyro:
+ * 
+ * Leaning forward = +y
+ * Tilting left    = -z
+ */
+
+// Dome
 #define DomeYEase .4 // Spead of front to back dome movement, higher == faster
 #define DomeXEase .7 // Speed of side to side domemovement, higher == faster
-#define domeSpeed 90 // Speed that the servos will move
+#define domeSpeed 150 // Speed that the servos will move
 
-// PID Controls
-
-//PID1 is for the side to side tilt
+// PID1 is for the side to side tilt
 double Pk1 = 14;
 double Ik1 = 0;
 double Dk1 = 0.0;
 double Setpoint1, Input1, Output1, Output1a;
-double roll = 0;
-double rollOffset = 0;
-
 PID PID1(&Input1, &Output1, &Setpoint1, Pk1, Ik1 , Dk1, DIRECT);
 
-// PID2 is for side to side stability
+// PID Setup - S2S stability
 double Pk2 = .5;
 double Ik2 = 0;
 double Dk2 = .01;
 double Setpoint2, Input2, Output2, Output2a;
-
-PID PID2(&Input2, &Output2, &Setpoint2, Pk2, Ik2 , Dk2, DIRECT);    // PID Setup - S2S stability
+PID PID2(&Input2, &Output2, &Setpoint2, Pk2, Ik2 , Dk2, DIRECT);
 
 // TODO:
+double roll = 0;
+double rollOffset = 0;
 int rcDome = 0;
 int rcFly = 0;
 double Joy2YPitch, Joy2YDirection, Joy2XDirection;
 int Joy2Ya, Joy2XLowOffset, Joy2XHighOffset, Joy2XLowOffsetA, Joy2XHighOffsetA, ServoLeft, ServoRight;
 double Joy2X, Joy2Y, LeftJoy2X, LeftJoy2Y, Joy2XEase, Joy2YEase, Joy2XEaseMap;
 double Joy2YEaseMap;
+
+// TODO:
+int s2sPot = 512;
 
 int potOffsetS2S = 3;
 int S2Spot;
@@ -94,10 +99,8 @@ VarSpeedServo rightServo;
 // BTS7960 Motor Drivers
 BTS7960 driveController(DRIVE_EN, DRIVE_L_PWM, DRIVE_R_PWM);
 BTS7960 s2sController(S2S_EN, S2S_L_PWM, S2S_R_PWM);
-BTS7960 flywheelController(FLY_EN, FLY_L_PWM, FLY_R_PWM);
+BTS7960 flywheelController(DRIVE_EN, FLY_L_PWM, FLY_R_PWM);
 
-// TODO:
-int s2sPot = 512;
 
 void setup()
 {
@@ -105,7 +108,8 @@ void setup()
   sbus_rx.Begin();
 
   // Sound
-  myDFPlayer.begin(Serial2);
+  Serial1.begin(9600);
+  myDFPlayer.begin(Serial1);
   myDFPlayer.volume(30);
   myDFPlayer.play(1);
 
@@ -120,7 +124,6 @@ void setup()
 
   pinMode(FLY_R_PWM, OUTPUT);
   pinMode(FLY_L_PWM, OUTPUT);
-  pinMode(FLY_EN, OUTPUT);
 
   pinMode(domeSpinPWM1, OUTPUT);
   pinMode(domeSpinPWM2, OUTPUT);
@@ -136,18 +139,17 @@ void setup()
   s2sController.Enable();
   flywheelController.Enable();
 
-  // PID Setup
-
-  // S2S SERVO
+  // S2S Servo PID
   PID1.SetMode(AUTOMATIC);
   PID1.SetOutputLimits(-255, 255);
   PID1.SetSampleTime(15);
 
-  // S2S Stability
+  // S2S Stability PID
   PID2.SetMode(AUTOMATIC);
   PID2.SetOutputLimits(-255, 255);
   PID2.SetSampleTime(15);
 }
+
 
 void loop()
 {
@@ -297,11 +299,11 @@ void loop()
       Setpoint2 -= S2SEase;
     }
 
-    #ifdef reverseS2SPot
-        S2Spot = map(analogRead(S2S_POT), 0, 1024, -135, 135);
-    #else
-        S2Spot = map(analogRead(S2S_POT), 0, 1024, 135, -135);
-    #endif
+  #ifdef reverseS2SPot
+      S2Spot = map(analogRead(S2S_POT), 0, 1024, -135, 135);
+  #else
+      S2Spot = map(analogRead(S2S_POT), 0, 1024, 135, -135);
+  #endif
 
     // PID2 is used to control the 'servo' control of the side to side movement.
     Input2 = roll + rollOffset;
@@ -313,10 +315,10 @@ void loop()
     Setpoint1 = map(constrain(Output2, -maxS2STilt, maxS2STilt), -maxS2STilt, maxS2STilt, maxS2STilt, -maxS2STilt);
     PID1.Compute();
 
-    if ((Output1 <= -1) && (Input1 > -maxS2STilt)) {
+    if ((Output1 <= -2) && (Input1 > -maxS2STilt)) {
       Output1a = abs(Output1);
       s2sController.TurnRight(Output1a);
-    } else if ((Output1 >= 1) && (Input1 < maxS2STilt)) {
+    } else if ((Output1 >= 2) && (Input1 < maxS2STilt)) {
       Output1a = abs(Output1);
       s2sController.TurnLeft(Output1a);
     } else {
